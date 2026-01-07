50f12af20dcefa1b6e5f8c047ea765d042cc54f7
martinordovas <martinordovas@gmail.com>
2026-01-07 18:45:52 +0100

    Arreglo responsive

--- scripts/script1.js
+++ scripts/script1.js
@@ -2,6 +2,18 @@ const barraNavegacion = document.getElementById('navbarPaginaPrincipal');
 const contenidoMenu = document.getElementById('menuNavbar');
 const altura = 50;
 
+function actualizarNavbar(){
+    if(window.scrollY >= altura || contenidoMenu.classList.contains('show')){
+        barraNavegacion.classList.add('cambioNavbar');
+    }else{
+        barraNavegacion.classList.remove('cambioNavbar');
+    }
+}
+
+window.onscroll = function() {
+    actualizarNavbar();
+};
+
 contenidoMenu.addEventListener('show.bs.collapse', function () {
     barraNavegacion.classList.add('cambioNavbar');
 });
