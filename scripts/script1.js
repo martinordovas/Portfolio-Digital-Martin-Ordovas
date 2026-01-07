0eab932bf8cd593d30b608a1dd629d69b39d7eb8
martinordovas <martinordovas@gmail.com>
2026-01-07 21:45:32 +0100

    Sobre mí y responsive finalizado

--- scripts/script1.js
+++ scripts/script1.js
@@ -1,16 +1,17 @@
 const barraNavegacion = document.getElementById('navbarPaginaPrincipal');
 const contenidoMenu = document.getElementById('menuNavbar');
+const dataSpyList = document.querySelectorAll('[data-bs-spy="scroll"]')
 const altura = 50;
 
-function actualizarNavbar(){
-    if(window.scrollY >= altura || contenidoMenu.classList.contains('show')){
+function actualizarNavbar() {
+    if (window.scrollY >= altura || contenidoMenu.classList.contains('show')) {
         barraNavegacion.classList.add('cambioNavbar');
-    }else{
+    } else {
         barraNavegacion.classList.remove('cambioNavbar');
     }
 }
 
-window.onscroll = function() {
+window.onscroll = function () {
     actualizarNavbar();
 };
 
@@ -21,5 +22,18 @@ contenidoMenu.addEventListener('show.bs.collapse', function () {
 contenidoMenu.addEventListener('hide.bs.collapse', function () {
     if (window.scrollY <= altura) {
         barraNavegacion.classList.remove('cambioNavbar');
+    }
+});
+
+window.addEventListener('activate.bs.scrollspy', function () {
+    const activeLink = document.querySelector('.navbar-nav .nav-link.active');
+    if (activeLink) {
+        const targetId = activeLink.getAttribute('href');
+        document.querySelectorAll('.dot').forEach(dot => {
+            dot.classList.remove('active');
+            if (dot.getAttribute('href') === targetId) {
+                dot.classList.add('active');
+            }
+        });
     }
 });
